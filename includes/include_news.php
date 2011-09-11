4fcac7deb7de4f54367b567b78d5153da36bc6ce
Adam Preston <adam.m.preston@gmail.com>
2011-09-11 18:15:06 -0500

    fixed a merge problem

--- includes/include_news.php
+++ includes/include_news.php
@@ -1,11 +1,9 @@
 
 
 
-<<<<<<< HEAD
+
 <div style="padding-top: 15px">
-=======
-<div>
->>>>>>> 839d309743f2e7c9b25bed4f55a857f6ac505526
+
 <ul class="clubnews">
 
 <?
@@ -16,25 +14,16 @@ if(isDebugEnabled(1) ) logMessage("include_news: the news is: $news");
 
 if( ! empty($news) ) { ?>
 	
-<<<<<<< HEAD
-<h2 >Club News</h2>
+<h2 style="padding-top: 15px">Club News</h2>
 <hr class="hrline"/>
 <li>
 <?=getTwitterStatus(get_twitterHandle(), "\\1"); ?>
 </li>
 <? } ?>
-</li>
-=======
-<h2>Club News</h2>
-<hr class="hrline"/>
-
-<?=getTwitterStatus(get_twitterHandle(), "\\1");
 
 
-}
-?>
 
->>>>>>> 839d309743f2e7c9b25bed4f55a857f6ac505526
+
 
 
 </ul>
