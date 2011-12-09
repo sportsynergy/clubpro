6821dd5df3620edc267d17ebc22c5cc73c7e3d0c
Adam Preston <adam.m.preston@gmail.com>
2011-12-09 02:10:18 -0600

    added more link files...forgot to earlier

---
+++ users/recent_activity.php
@@ -0,0 +1,27 @@
+<?
+
+include("../application.php");
+
+// set some variables from the request
+$start = $_REQUEST["start"];
+$siteid = $_REQUEST["siteid"];
+
+	
+	if( isDebugEnabled(1) ) logMessage("getting recent activity for siteid $siteid and start $start");
+
+	header("content-type: application/json");
+	if( isset($start) && isset($siteid)){
+		
+		$result = getRecentSiteActivityBlock($siteid, $start);
+		$all_data = array();
+		while($recentActivity = db_fetch_object($result)){
+			 $all_data[] = $recentActivity;
+		}
+		echo json_encode($all_data);
+	}
+	
+	
+
+
+
+?>
