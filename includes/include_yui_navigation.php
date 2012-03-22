a0d3ecd578172144723392b34cc374bb80856d5e
Adam Preston <adam.m.preston@gmail.com>
2012-03-21 21:09:36 -0500

    Added Rankings MOve the Rankings menu option to the users navigation bar.  Back by popular demand.

--- includes/include_yui_navigation.php
+++ includes/include_yui_navigation.php
@@ -68,6 +68,10 @@
           </div>
         </div>
       </li>
+   <?	if(isLadderRankingScheme() ){ ?>
+          <li class="yuimenubaritem first-of-type"><a class="yuimenuitemlabel" href="<?=$_SESSION["CFG"]["wwwroot"]?>/users/player_rankings.php">Rankings</a></li>
+     <?php }?>
+
       <?php if( get_roleid() == 2 ) { ?>
       <li class="yuimenubaritem"><a class="yuimenubaritemlabel" href="#">Tools</a>
         <div id="administration" class="yuimenu">
@@ -79,11 +83,7 @@
               <li class="yuimenuitem"><a class="yuimenuitemlabel" href="<?=$_SESSION["CFG"]["wwwroot"]?>/admin/web_ladder_registration.php">Box League Setup</a></li>
               <li class="yuimenuitem"><a class="yuimenuitemlabel" href="<?=$_SESSION["CFG"]["wwwroot"]?>/admin/policy_preferences.php">Club Preferences</a></li>
               <li class="yuimenuitem"><a class="yuimenuitemlabel" href="<?=$_SESSION["CFG"]["wwwroot"]?>/admin/report_scores.php">Report Scores</a></li>
-              <?php 
-			  //At least let admins see this.  This isn't displayed otherwise
-			  if(isLadderRankingScheme()){ ?>
-              <li class="yuimenuitem"><a class="yuimenuitemlabel" href="<?=$_SESSION["CFG"]["wwwroot"]?>/users/player_rankings.php">Rankings</a></li>
-              <?php }?>
+             
               <li class="yuimenuitem"><a class="yuimenuitemlabel" href="<?=$_SESSION["CFG"]["wwwroot"]?>/admin/club_events.php">Club Events</a></li>
               <li class="yuimenuitem"><a class="yuimenuitemlabel" href="<?=$_SESSION["CFG"]["wwwroot"]?>/admin/club_reports.php">Club Reports</a></li>
             </ul>
