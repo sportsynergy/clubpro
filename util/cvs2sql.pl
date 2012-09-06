014c5bf3c5213092c9f71bd31fc5c69d809f6402
Adam Preston <adam.m.preston@gmail.com>
2012-09-06 15:41:13 -0600

    beefed up the script for adding new users (from a spreadsheet)

--- util/cvs2sql.pl
+++ util/cvs2sql.pl
@@ -13,11 +13,36 @@
 # usage: perl cvs2sql.pl ~/Downloads/tblUserRankings.sql > output.sql
 
 
+$start = 5425;
+$password = "8a48bca7b89dc6450afd4dcee81a35b4";
+$clubid = 47;
+$roleid = 1;
+$siteid = 89;
+$ranking = 3;
+
 open (MYFILE, $ARGV[0]);
 while (<MYFILE>) {
 	chomp;
-	#userid, courttypeid, ranking, hot, usertype, lastmodified
+	#first, last, email, gender
 	@personal = split(/,/, $_);
-	print "INSERT INTO `tblUserRankings` VALUES (@personal[0],@personal[1],@personal[2],@personal[3],@personal[4],@personal[5]);\n";
+	
+	$gender = @personal[3] eq 'M' ? 0 : 1;
+
+	#users
+	print "INSERT INTO `tblUsers` (`userid`, `username`, `firstname`, `lastname`, `email`, `password`, `gender`)  VALUES ($start,'@personal[2]','@personal[1]','@personal[0]','@personal[2]','$password', $gender);\n";
+	
+	#clubuser
+	#print "INSERT INTO `tblClubUser` (`userid`, `clubid`, `roleid`, `recemail`)  VALUES ($start,'$clubid','$roleid','n');\n";
+		
+	#ranking
+	#print "INSERT INTO `tblUserRankings` (`userid`, `courttypeid`, `ranking`)  VALUES ($start,2,'$ranking');\n";
+	#print "INSERT INTO `tblUserRankings` (`userid`, `courttypeid`, `ranking`)  VALUES ($start,3,'$ranking');\n";
+	
+	#auth
+#	print "INSERT INTO `tblkupSiteAuth` (`userid`, `siteid`)  VALUES ($start,$siteid);\n\n";
+	
+	
+	
+	++$start;
 }
 close (MYFILE);
