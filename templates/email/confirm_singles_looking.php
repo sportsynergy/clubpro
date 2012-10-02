3032204d94aa5a2bed1b238be77961094a60b071
Adam Preston <adam.m.preston@gmail.com>
2012-10-02 13:51:46 -0600

    removed potentially confusing text

--- templates/email/confirm_singles_looking.php
+++ templates/email/confirm_singles_looking.php
@@ -1,1 +1,1 @@
-Your reservation on $var->courtname on $var->time has been made sucessfully.  As far as your opponent goes, well we are still working on that.  Emails are being sent out to fellow club members looking to fill this spot.
+Your reservation on $var->courtname on $var->time has been made successfully.  As far as your opponent goes, well we are still working on that.  
