c3c8a3517eee720710919c32d2e45850c3ba5b04
Adam Preston <adam.m.preston@gmail.com>
2016-09-07 10:04:39 -0600

    addd doubles and matchtype to ical export

--- api/ical.php
+++ api/ical.php
@@ -46,29 +46,55 @@ $vCalendar->setTimezone($vTimezone);
 
 
 // Get singles
-$query = "SELECT tblReservations.time,tblCourts.courtname, tblClubs.clubname, tblSportType.sportname
+$query = " (SELECT tblReservations.time,tblCourts.courtname, tblClubs.clubname, tblSportType.sportname, tblMatchType.name AS 'matchtype'
 			FROM tblReservations 
 			INNER JOIN tblkpUserReservations ON tblReservations.reservationid = tblkpUserReservations.reservationid
 			INNER JOIN tblCourts ON tblReservations.courtid = tblCourts.courtid
 			INNER JOIN tblClubs ON tblCourts.clubid = tblClubs.clubid
 			INNER JOIN tblCourtType ON tblCourts.courttypeid = tblCourtType.courttypeid
 			INNER JOIN tblSportType ON tblCourtType.sportid = tblSportType.sportid
+			INNER JOIN tblMatchType ON tblReservations.matchtype = tblMatchType.id
 			WHERE tblkpUserReservations.userid = '$userid'
 			AND tblReservations.usertype = 0 
 			AND tblReservations.time > $monthago
 			AND tblReservations.time < $intwomonths
 			AND tblReservations.enddate is NULL
-			ORDER by tblReservations.time DESC
-			LIMIT 500";
-
-// union doubles
-
 			
+			LIMIT 500
+			)
+			UNION ALL
+
+			(SELECT tblReservations.time,tblCourts.courtname, tblClubs.clubname, tblSportType.sportname,tblMatchType.name AS 'matchtype'
+			FROM tblReservations 
+			INNER JOIN tblkpUserReservations ON tblReservations.reservationid = tblkpUserReservations.reservationid
+			INNER JOIN tblCourts ON tblReservations.courtid = tblCourts.courtid
+			INNER JOIN tblClubs ON tblCourts.clubid = tblClubs.clubid
+			INNER JOIN tblCourtType ON tblCourts.courttypeid = tblCourtType.courttypeid
+			INNER JOIN tblSportType ON tblCourtType.sportid = tblSportType.sportid 
+			INNER JOIN tblMatchType ON tblReservations.matchtype = tblMatchType.id
+			WHERE tblkpUserReservations.userid IN (SELECT tblTeams.teamid
+		FROM tblTeams INNER JOIN tblkpTeams ON tblTeams.teamid = tblkpTeams.teamid
+		WHERE tblkpTeams.userid ='$userid')
+			AND tblReservations.usertype = 1 
+		    AND tblkpUserReservations.usertype = 1
+		    AND tblReservations.time > $monthago
+			AND tblReservations.time < $intwomonths
+			AND tblReservations.enddate is NULL
+			
+			LIMIT 500
+			) ORDER by time DESC ";
+
+	
 
 // Get all reservations for the user including: court name, time and
 $result = db_query($query);
 
 while($array = db_fetch_array($result)) {    
+	
+	$matchtype = '';
+	if ($array['matchtype'] == 'lesson') {
+		$matchtype = ucfirst ( $array['matchtype'] ).' ';
+	}
 	
 	$start = gmdate("Y-m-d H:i",$array['time']);
 	$endtime = $array['time'] + 3600;
@@ -80,7 +106,7 @@ while($array = db_fetch_array($result)) {
 	$vEvent->setDtStart(new DateTime($start,$dtz))
     ->setDtEnd(new DateTime($end,$dtz))
     ->setLocation($array['clubname'])
-    ->setSummary($array['sportname'].' on '.$array['courtname']);
+    ->setSummary($array['sportname'].' '.$matchtype.'on '.$array['courtname']);
 	
 	$vEvent->setUseTimezone(true);
 
