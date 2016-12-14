18013c629b68dd4795824a6c514dea5c10f15b72
Adam Preston <adam.m.preston@gmail.com>
2016-12-14 11:27:43 -0700

    add player names in ical api

--- api/ical.php
+++ api/ical.php
@@ -1,9 +1,7 @@
 <?php
 
 /**
-* Class and Function List:
-* Function list:
-* Classes list:
+ Usage: https://www.sportsynergy.net/api/ical.php?user=1237
 */
 
 require '../vendor/autoload.php';
@@ -46,8 +44,12 @@ $vCalendar->setTimezone($vTimezone);
 
 
 // Get singles
-$query = " (SELECT tblReservations.time,tblCourts.courtname, tblClubs.clubname, tblSportType.sportname, tblMatchType.name AS 'matchtype', tblReservations.duration
-			FROM tblReservations 
+$query = " (SELECT tblReservations.time,tblCourts.courtname, tblClubs.clubname, tblSportType.sportname, tblMatchType.name AS 'matchtype', tblReservations.duration,
+(SELECT concat(u.firstname,' ',u.lastname) FROM tblUsers u
+	INNER JOIN tblkpUserReservations p ON u.userid = p.userid
+	WHERE p.reservationid = tblReservations.reservationid
+AND p.userid != '1023') as 'partner'
+FROM tblReservations
 			INNER JOIN tblkpUserReservations ON tblReservations.reservationid = tblkpUserReservations.reservationid
 			INNER JOIN tblCourts ON tblReservations.courtid = tblCourts.courtid
 			INNER JOIN tblClubs ON tblCourts.clubid = tblClubs.clubid
@@ -55,16 +57,15 @@ $query = " (SELECT tblReservations.time,tblCourts.courtname, tblClubs.clubname, 
 			INNER JOIN tblSportType ON tblCourtType.sportid = tblSportType.sportid
 			INNER JOIN tblMatchType ON tblReservations.matchtype = tblMatchType.id
 			WHERE tblkpUserReservations.userid = '$userid'
-			AND tblReservations.usertype = 0 
+			AND tblReservations.usertype = 0
 			AND tblReservations.time > $monthago
 			AND tblReservations.time < $intwomonths
 			AND tblReservations.enddate is NULL
-			
 			LIMIT 500
 			)
 			UNION ALL
 
-			(SELECT tblReservations.time,tblCourts.courtname, tblClubs.clubname, tblSportType.sportname,tblMatchType.name AS 'matchtype', tblReservations.duration
+			(SELECT tblReservations.time,tblCourts.courtname, tblClubs.clubname, tblSportType.sportname,tblMatchType.name AS 'matchtype', tblReservations.duration,'' as 'partner'
 			FROM tblReservations 
 			INNER JOIN tblkpUserReservations ON tblReservations.reservationid = tblkpUserReservations.reservationid
 			INNER JOIN tblCourts ON tblReservations.courtid = tblCourts.courtid
@@ -85,7 +86,6 @@ $query = " (SELECT tblReservations.time,tblCourts.courtname, tblClubs.clubname, 
 			) ORDER by time DESC ";
 
 
-
 // Get all reservations for the user including: court name, time and
 $result = db_query($query);
 
@@ -96,12 +96,21 @@ while($array = db_fetch_array($result)) {
 		$matchtype = ucfirst ( $array['matchtype'] ).' ';
 	}
 
+	$with = '';
+	if( isset($array['partner']) ){
+		$with = "with ".$array['partner'];
+	}
+
+
 	$start = gmdate("Y-m-d H:i",$array['time']);
 	
 	$duration = 3600;
 	if( isset($array['duration']) ){
 		$duration = $array['duration'];
 	}
+
+
+
 
 	$endtime = $array['time'] + $duration;
 	
@@ -112,7 +121,7 @@ while($array = db_fetch_array($result)) {
 	$vEvent->setDtStart(new DateTime($start,$dtz))
     ->setDtEnd(new DateTime($end,$dtz))
     ->setLocation($array['clubname'])
-    ->setSummary($array['sportname'].' '.$matchtype.'on '.$array['courtname']);
+    ->setSummary($array['sportname'].' '.$matchtype.'on '.$array['courtname'].' '.$with);
 	
 	$vEvent->setUseTimezone(true);
 
