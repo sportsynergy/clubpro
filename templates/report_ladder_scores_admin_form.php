4f25ddccabb55e457e3062eca6aefbe8232d223f
Adam Preston <adam.m.preston@outlook.com>
2023-09-12 18:02:22 -0600

    blank ladder report score screen

--- templates/report_ladder_scores_admin_form.php
+++ templates/report_ladder_scores_admin_form.php
@@ -10,35 +10,10 @@
           </span></td>
       </tr>
       <tr>
-        <td><table width="400" class="borderless">
-            <tr>
-              <td class="label">Member Name:</td>
-              <td><input type="text" name="searchname" size="25" value="<? pv($searchname)?>">
-                <?err($errors->searchname)?></td>
-            </tr>
-            <tr>
-              <td colspan="2"><span class="normal"> Search for the first or last name of a member. *Note partial string are supported. <span style="font-style: italic;"> i.e. Smi for Smith or Pet for Peter</span> </span></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td><input type="button" name="submit" value="Search" id="submitbutton"></td>
-            </tr>
-          </table></td>
-      </tr>
-    </table>
+        <td>
+            <table width="400" class="borderless">
+            <td>Coming soon...</td>
+            </table>
   </form>
   <div style="height: 30px"></div>
 </div>
-<script type="text/javascript">
-document.entryform.searchname.focus();
-YAHOO.example.init = function () {
-    YAHOO.util.Event.onContentReady("formtable", function () {
-        var oSubmitButton1 = new YAHOO.widget.Button("submitbutton", { value: "submitbutton1value" });
-        oSubmitButton1.on("click", onSubmitButtonClicked);   
-    });
-
-} ();
-function onSubmitButtonClicked(){
-	submitForm('entryform');
-}
-</script> 
