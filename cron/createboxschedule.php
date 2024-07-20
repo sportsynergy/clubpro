9d6084d3b29d00ca2bd3bf138bcb06d2ca4949c0
Adam Preston <adam.m.preston@outlook.com>
2024-07-20 09:46:00 -0600

    #50 calculate who hasn't played who

--- cron/createboxschedule.php
+++ cron/createboxschedule.php
@@ -38,21 +38,52 @@ class CreateBoxLeagueSchedule {
             if (isDebugEnabled(1)) logMessage("CreateBoxLeagueSchedule: Processing ". $box_array['boxname'] . "(".$box_array['ladderid'].") "); 
         
             // For each person in the league, get the number of matches player for other players
-            $box_query = "SELECT concat(tU.firstname,' ',tU.lastname) as full_name
+            $box_query = "SELECT concat(tU.firstname,' ',tU.lastname) as full_name, tU.userid
                             FROM tblkpBoxLeagues
                             INNER JOIN clubpro_main.tblUsers tU on tblkpBoxLeagues.userid = tU.userid
                             WHERE boxid = ".$box_array['boxid'];
             $bresult = db_query($box_query);
             
+            $all_players = array();
+            
+
+            while($boxplayer_array = mysqli_fetch_array($bresult) ){
+                //put all userids into an array
+                array_push($all_players, $boxplayer_array['userid']);
+            }
+
+            mysqli_data_seek($bresult,0);
             while($boxplayer_array = mysqli_fetch_array($bresult) ){
 
                 if (isDebugEnabled(1)) logMessage("CreateBoxLeagueSchedule: Getting results for ". $boxplayer_array['full_name'] ); 
+                
+                $already_played = array();
 
-                //put all userids into an array
-        
+                // add the players this person has beat to the already played array
+                $loser_query = "SELECT loserid FROM tblLadderMatch WHERE league = TRUE AND winnerid = 15272";
+                $loser_result = db_query($loser_query);
+                while($loserid_array = mysqli_fetch_array($loser_result) ){
+                    array_push($already_played, $loserid_array['loserid']);
+                }
+
+                // add the players this person has lost to to the already played array
+                $winner_query = "SELECT winnerid FROM tblLadderMatch WHERE league = TRUE AND loserid = 15272";
+                $winner_result = db_query($loser_query);
+                while($loserid_array = mysqli_fetch_array($loser_result) ){
+                    array_push($already_played, $loserid_array['winnerid']);
+                }
+
+                $havent_played = array_diff($all_players, $already_played);
+                foreach ($havent_played as &$playerid) {
+                    if (isDebugEnabled(1)) logMessage("\tStill needs to play $playerid" ); 
+    
+                }
+                
             }
 
-            // look through ladder matches that are box leagues, (need to add a column for this on laddermatch table to indicate that it was a box league match)
+           
+            
+
             
             //order of scheduling prioritizing players who haven't played first
 
