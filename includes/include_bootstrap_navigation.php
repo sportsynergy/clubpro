7f0f8fe9a4594724cc88c0ce02fdd00c6a65f589
Adam Preston <adam.m.preston@outlook.com>
2025-12-14 07:48:35 -0700

    better nav bar

--- includes/include_bootstrap_navigation.php
+++ includes/include_bootstrap_navigation.php
@@ -1,9 +1,12 @@
-<nav class="navbar navbar-expand navbar-light bg-white" >
+ <nav class="navbar navbar-expand-lg navbar-light bg-white">
   <div class="container-fluid">
-       <a class="navbar-brand" href="<?=$_SESSION["CFG"]["wwwroot"]?>/clubs/<?=get_sitecode()?>/">
+     <a class="navbar-brand" href="<?=$_SESSION["CFG"]["wwwroot"]?>/clubs/<?=get_sitecode()?>/">
         <img src="<?=$_SESSION["CFG"]["wwwroot"]?>/clubs/<?=get_sitecode()?>/logo.png" alt="" width="80" height="80" class="d-inline-block align-text-top">
     </a>
-    
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         
@@ -13,7 +16,7 @@
         <li class="nav-item">
           <a class="nav-link" href="<?=$_SESSION["CFG"]["wwwroot"]?>/clubs/<?echo get_sitecode()?>/" id="bookings-navlink">Bookings</a>
         </li>
-        
+
         <? if(isLadderRankingScheme() || isJumpLadderRankingScheme() ) {?>
            <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="rankings-navlink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -103,10 +106,12 @@
                 
             </ul>
           <? } ?>
+
+
         
       </ul>
-    
-      <span class="navbar-text">
+
+        <span class="navbar-text">
         <a href="<?=$_SESSION["CFG"]["wwwroot"]?>/logout.php" title="<? p($_SESSION["user"]["firstname"] . " " . $_SESSION["user"]["lastname"]) ?>">Logout</a>
       </span>
 
