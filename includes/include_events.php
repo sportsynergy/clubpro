e13395fa166400cddab7a125330c01bc2dfe3d54
Adam Preston <adam.m.preston@outlook.com>
2026-01-03 08:39:37 -0600

    fix event links for good

--- includes/include_events.php
+++ includes/include_events.php
@@ -15,15 +15,19 @@ if(mysqli_num_rows($clubEventsResult) > 0){ ?>
 <?
 	
 while($clubEvent = mysqli_fetch_array($clubEventsResult)){ ?>
+	
+	<li>
+	<a href="javascript:safeSubmit('loadClubEventForm<?=$clubEvent['id']?>')" >
+		<?=$clubEvent['name']?>
+	</a> 
+	<div class="italic">
+		<?=formatDateString($clubEvent['eventdate'])?> 
+</div>
+	</li>
 
 	 <form name="loadClubEventForm<?=$clubEvent['id']?>" action="<?=$_SESSION["CFG"]["wwwroot"]?>/users/club_event.php" method="post">
-           <input type="hidden" name="clubeventid" value="<?=$clubEvent['id'] ?>">
-		   <input type="hidden" name="clubid" value="<?=get_clubid() ?>">
-		   <input type="submit" value="<?=$clubEvent['name']?>" name="submit" class="buttonlink" >	
+           <input type="hidden" name="clubeventid" value="<?=$clubEvent['id'] ?>">	
      </form>
-	 <div class="italic">
-		<?=formatDateString($clubEvent['eventdate'])?> 
-	</div>
 <? }} ?>
 
 
