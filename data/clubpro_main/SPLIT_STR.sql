633dc965e44505a68fb08c0754a0da0a8cb03745
Adam Preston <adam.m.preston@outlook.com>
2026-03-05 12:02:29 -0700

    updates for recsms

--- data/clubpro_main/SPLIT_STR.sql
+++ data/clubpro_main/SPLIT_STR.sql
@@ -1,6 +1,6 @@
 create
-    definer = root@localhost 
-    function clubpro_main.SPLIT_STR(x varchar(255), delim varchar(12), pos int) returns varchar(255) DETERMINISTIC
+    definer = root@localhost function clubpro_main.SPLIT_STR(x varchar(255), delim varchar(12), pos int) returns varchar(255)
     RETURN REPLACE(SUBSTRING(SUBSTRING_INDEX(x, delim, pos),
        LENGTH(SUBSTRING_INDEX(x, delim, pos -1)) + 1),
        delim, '');
+
